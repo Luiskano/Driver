c682c6b15fc11cbf84f9e2e1db031fd0686c9aff
Luiskano <65258636+Luiskano@users.noreply.github.com>
2020-05-20 15:03:55 -0500

    sprint  1
    implementacion del mapa

--- Login/app/src/main/java/com/example/login/JobMap.kt
+++ Login/app/src/main/java/com/example/login/JobMap.kt
@@ -1,45 +1,69 @@
 package com.example.login
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class JobMap : AppCompatActivity(), OnMapReadyCallback {
+class JobMap : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
     private lateinit var mMap: GoogleMap
 
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_map)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.Map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val mexicoSLP = LatLng(22.1564699, -100.9855409)
-        mMap.addMarker(MarkerOptions().position(mexicoSLP).title("San Luis Potosi"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mexicoSLP, 13F))
         mMap.uiSettings.isZoomControlsEnabled = true;
+        mMap.setOnMarkerClickListener(this)
+        setUpMap()
     }
+
+    private fun setUpMap(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this){location ->
+
+            if (location != null){
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 15F))
+            }
+        }
+
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
 }
